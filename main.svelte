523646163c1a91a6fd4fb5ce03f0bdca64627b4b
Rosano Coutinho <pub@xeari.com>
2020-11-22 13:16:46 -0300

    Extract OLSKLocalized to OLSKInternational

--- main.svelte
+++ main.svelte
@@ -18,11 +18,7 @@ export let OLSKMasterListFilterFieldAutofocus = false;
 
 export let OLSKMasterItemClass = '';
 
-import OLSKInternational from 'OLSKInternational';
-const OLSKLocalized = function(translationConstant) {
-	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`)[window.OLSKPublicConstants('OLSKSharedPageCurrentLanguage')]);
-};
-
+import { OLSKLocalized } from 'OLSKInternational';
 import { OLSK_TESTING_BEHAVIOUR } from 'OLSKTesting'
 
 const mod = {
