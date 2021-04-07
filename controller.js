c21260522f7cd32d065e514e21d2f08bbb7ccc1d
Rosano Coutinho <pub@xeari.com>
2021-04-07 09:45:41 -0300

    Tidy

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteMethod: 'get',
 		OLSKRouteSignature: 'OLSKMasterListStubRoute',
 		OLSKRouteFunction (req, res, next) {
-			return res.render(require('path').join(__dirname, 'stub-view'));
+			return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'stub-view'));
 		},
 		OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
 	}];
